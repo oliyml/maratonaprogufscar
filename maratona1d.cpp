84b597e98efbe9bee9de4606ba375f9524924183
oli! <rmgoncalves@estudante.ufscar.br>
2026-04-05 19:40:20 -0300

    Create maratona1d.cpp

---
+++ maratona1d.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    float A, B;
+    cin >> A >> B;
+
+    cout << setprecision(2) << fixed << A/B << endl;
+    
+    return 0;    
+}
